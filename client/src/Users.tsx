755c69b853e8e35fde23dc27c96a40f4c808e9f2
Karolina Oleksiewicz <karolinaoleksiewicz1@gmail.com>
2022-06-22 15:39:33 +0200

    not finished

--- client/src/Users.tsx
+++ client/src/Users.tsx
@@ -1,16 +1,54 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
+
+type User = {
+    id: number,
+    name: string,
+    email: string,
+    address: Address,
+}
+
+type Address = {
+    street: string,
+}
 
 export const Users: FC = () => {
-  const sort = (key: 'id' | 'name') => {
-  }
+    const [users, setUsers]= useState<User[]>([])
+    const [currentUsers, setCurrentUsers] = useState([]);
+  // @ts-ignore
+    useEffect(async() => {
+     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+     const users = await response.json ()
+     setUsers(users);
+    }, []);
+    
+    const [sorting, setSorting] = useState({ field: 'name', ascending: false })
+    const sort = ( key: string, ascending: boolean) => {
+        setSorting ({ field: 'name', ascending: ascending});
+    }
 
-  return <table>
-    <tr>
-      <th>id</th>
-      <th>name</th>
+    useEffect(() => {
+        const currentUsersCopy = [...users];
+        currentUsersCopy.sort((a: string, b: string) => {
+            return a[sorting.field].localeCompare(b[sorting.field]);
+        });
+    }, [users, sort]);
+
+    return <table>
+      <tr>
+      <th >id</th>
+      <th onClick={() => sort.name}>name</th>
       <th>email</th>
       <th>street</th>
     </tr>
+
+    {users.map(user => {
+      return  <tr>
+        <td>{user.id}</td>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.address.street}</td>
+      </tr>
+    })}
 
   </table>
 }
--- server/src/Routes.ts
+++ server/src/Routes.ts
@@ -5,6 +5,20 @@ export class Routes {
     app.get("/hello", (req, res) => {
       res.json({ hello: 'world' });
     });
+    app.get("/users", (req, res) => {
+      res.json([{
+        "id": 1,
+        "name": "Leanne Graham",
+        "email": "Sincere@april.biz",
+        "street": "street"
+      },
+        {
+          "id": 2,
+          "name": "Karolina",
+          "email": "karolina.oleksiewicz1@gmail.com",
+          "street": "Akacjowa"
+        }])
+    });
   }
 }
 
